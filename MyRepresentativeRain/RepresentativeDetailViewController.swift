a45e2ee308dccfed78dbb0e00b08c7a7e4e3a07c
Zach Steed <zsteed21@gmail.com>
2016-03-17 11:59:24 -0600

    Wiring Views to data

--- MyRepresentativeRain/RepresentativeDetailViewController.swift
+++ MyRepresentativeRain/RepresentativeDetailViewController.swift
@@ -10,26 +10,26 @@ import UIKit
 
 class RepresentativeDetailViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var districtLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var websiteLabel: UILabel!
+    
+    var representative: Representative?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func updateWithRepresentative(representative:Representative) {
+        self.representative = representative
+        
+        self.nameLabel.text = representative.name
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
--- MyRepresentativeRain/RepresentativeSearchViewController.swift
+++ MyRepresentativeRain/RepresentativeSearchViewController.swift
@@ -8,24 +8,64 @@
 
 import UIKit
 
-class RepresentativeSearchViewController: UIViewController {
+class RepresentativeSearchViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var states = ["AK", "AL", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
 
+    @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        RepresentativeController.searchForRepresentativeByState("UT") { (representatives) -> Void in
-            
+        self.activityIndicator.hidden = true
+    }
+    
+    @IBAction func searchButtonTapped(sender: UIButton) {
+        self.activityIndicator.hidden = false
+        self.activityIndicator.startAnimating()
+        let rowIndex = self.pickerView.selectedRowInComponent(0)
+        
+        let selectedState = self.states[rowIndex]
+        
+        RepresentativeController.searchForRepresentativeByState(selectedState) { (representatives) -> Void in
+            if let representatives = representatives {
+                if representatives.count > 0 {
+                    RepresentativeController.sharedInstance.myRepresentatives = representatives
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        self.performSegueWithIdentifier("toListView", sender: self)
+                        self.activityIndicator.stopAnimating()
+                        self.activityIndicator.hidden = true
+                    })
+                } else {
+                    print("Error searching for reps")
+                }
+            }
         }
     }
     
+    //MARK: - PickerView Data Source
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.states.count
+    }
 
-    /*
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //MARK: - PickerView Delegate
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.states[row]
+    }
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "toListView" {
+            let _ = segue.destinationViewController as! RepresentativeListTableViewController
+        }
     }
-    */
+    
 
 }
